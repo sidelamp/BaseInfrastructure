b2fef6a18554e23215c635aa759c1b5f189b342a
Muxa <gilfmuxa@gmail.com>
2023-02-28 12:57:39 +0400

    fix

--- Assets/Codebase/Core/Pool/ObjectOfPool.cs
+++ Assets/Codebase/Core/Pool/ObjectOfPool.cs
@@ -6,5 +6,10 @@ namespace Assets.Codebase.Core.Pool
     public abstract class ObjectOfPool : MonoBehaviour
     {
         public event Action<ObjectOfPool> OnDispose;
+
+        protected void CallDispose()
+        {
+            OnDispose?.Invoke(this);
+        }
     }
 }
