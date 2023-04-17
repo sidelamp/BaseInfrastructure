d0b39b7079aea7b1e37950a2cb535e4ae31842b6
Mishka <52670582+sidelamp@users.noreply.github.com>
2023-04-17 10:29:42 +0400

    Update CanvasService.cs
    Add stretch for popups

--- Assets/Codebase/Infrastructure/Services/CanvasService.cs
+++ Assets/Codebase/Infrastructure/Services/CanvasService.cs
@@ -21,9 +21,20 @@ namespace Codebase.Infrastructure.Services
 
             popup.transform.SetParent(_canvas.transform);
             popup.GetComponent<Canvas>().overrideSorting = true;
+            
+            SetStretch(popup);
         }
 
         public T GetPopup<T>() where T : PopupBase =>
             _popups.FirstOrDefault(p => p is T) as T;
+            
+        private void SetStretch(PopupBase popup)
+        {
+            var rectTransform = popup.GetComponent<RectTransform>();
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.sizeDelta = Vector2.zero;
+            rectTransform.anchoredPosition = Vector2.zero;
+        }
     }
 }
